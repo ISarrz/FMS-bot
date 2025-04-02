1c9fb960499d6a96147f92d810913f4d4b334403
German Zlobin <gezlobin@edu.hse.ru>
2025-04-02 21:29:02 +0300

    web fix 1.7.1

--- modules/database_updater/downloader.py
+++ modules/database_updater/downloader.py
@@ -11,108 +11,94 @@ import os
 from modules.logger.logger import async_logger, logger
 
 
-class Web:
-    def __init__(self, browser, page):
-        self.browser = browser
-        self.page = page
+# playwright install firefox
+@async_logger
+async def run(hide=True):
+    async with async_playwright() as playwright:
+        site_address = get_config_field('site_address')
+        user_login = get_config_field('site_login')
+        user_password = get_config_field('site_password')
 
-    @classmethod
-    async def create(cls, hide=True):
-        browser, page = await cls.async_init(hide)
-        web = Web(browser, page)
-        await web.login()
-        return web
-
-    @staticmethod
-    async def async_init(hide):
-        playwright_t = await async_playwright().start()
-
-        browser = await playwright_t.firefox.launch(headless=hide)
-
+        browser = await playwright.firefox.launch(headless=hide)
         page = await browser.new_page()
-        return browser, page
 
-    async def login(self):
-        await self.page.goto(get_config_field('site_address'))
-
-        login_field = self.page.locator('input[autocomplete="username"]')
-        password_field = self.page.locator('input[type="password"]')
-        sing_in_button = self.page.locator("button.button--red")
-
-        await login_field.fill(get_config_field('site_login'))
-        await password_field.fill(get_config_field('site_password'))
-        await sing_in_button.click()
+        await login(page, site_address, user_login, user_password)
+        await go_to_news(page)
+        await expand_news(page)
+        await expand_news(page)
+        current_news = await get_current_news(page)
+        await download_files(page, current_news)
+        await browser.close()
 
 
-    async def download_timetable(self):
-        await self.go_to_news()
-        await self.expand_news()
-        await self.expand_news()
-        current_news = await self.get_current_news()
-        await self.download_files(current_news)
+async def login(page, site_address, user_login, user_password):
+    await page.goto(site_address)
 
-    async def go_to_news(self):
-        news_button = self.page.locator('i[class="fal fa-fw fa-bullhorn"]')
-        await news_button.click()
+    login_field = page.locator('input[autocomplete="username"]')
+    password_field = page.locator('input[type="password"]')
+    sing_in_button = page.locator("button.button--red")
 
-    async def expand_news(self):
-        news = self.page.locator('div.board-item')
-        await expect(news).not_to_have_count(0)
-        number = await news.count()
-        for index in range(number - 1, -1, -1):
-            cur = news.nth(index)
-            news_is_active = await cur.locator('div.board-item__content').is_visible()
-            if not news_is_active:
-                roll_down = cur.locator('div.board-item__title')
-                await roll_down.click()
+    await login_field.fill(user_login)
+    await password_field.fill(user_password)
+    await sing_in_button.click()
 
-    async def get_current_news(self):
-        dates_pool = get_current_week() + get_next_week()
-        downloaded_dates = [date.replace(".xlsx", "") for date in os.listdir(downloads_path)]
-        downloaded_dates += [date.replace(".xlsx", "") for date in os.listdir(parsed_files_path)]
-        string_dates_pool = [date.strftime('%d.%m') for date in dates_pool if
-                             date.strftime('%d.%m') not in downloaded_dates]
 
-        current_news = []
-        news = self.page.locator('div.board-item')
-        number = await news.count()
-        for i in range(number):
-            cur_news = news.nth(i)
-            text = await cur_news.inner_text()
-            text = text.lower()
-            if "расписание" not in text or "урок" not in text:
-                continue
+async def go_to_news(page):
+    news_button = page.locator('i[class="fal fa-fw fa-bullhorn"]')
+    await news_button.click()
 
-            file = cur_news.locator('span[class="button__title"]')
-            file_name = await file.inner_text()
-            file_name = file_name.strip().replace('.xlsx', '')
 
-            if file_name in string_dates_pool:
-                current_news.append(cur_news)
+async def expand_news(page):
+    news = page.locator('div.board-item')
+    await expect(news).not_to_have_count(0)
+    number = await news.count()
+    for index in range(number - 1, -1, -1):
+        cur = news.nth(index)
+        news_is_active = await cur.locator('div.board-item__content').is_visible()
+        if not news_is_active:
+            roll_down = cur.locator('div.board-item__title')
+            await roll_down.click()
 
-        return current_news
 
-    async def download_files(self, dates):
-        for date in dates:
-            async with self.page.expect_download() as download_info:
-                button = date.locator('a.button--purple').first
-                await button.click()
-                download = await download_info.value
+async def get_current_news(page):
+    dates_pool = get_current_week() + get_next_week()
+    downloaded_dates = [date.replace(".xlsx", "") for date in os.listdir(downloads_path)]
+    downloaded_dates += [date.replace(".xlsx", "") for date in os.listdir(parsed_files_path)]
+    string_dates_pool = [date.strftime('%d.%m') for date in dates_pool if
+                         date.strftime('%d.%m') not in downloaded_dates]
 
-            await download.save_as(os.path.join(downloads_path, download.suggested_filename))
+    current_news = []
+    news = page.locator('div.board-item')
+    number = await news.count()
+    for i in range(number):
+        cur_news = news.nth(i)
+        text = await cur_news.inner_text()
+        text = text.lower()
+        if "расписание" not in text or "урок" not in text:
+            continue
 
-    async def close(self):
-        self.browser.close()
+        file = cur_news.locator('span[class="button__title"]')
+        file_name = await file.inner_text()
+        file_name = file_name.strip().replace('.xlsx', '')
 
-    # playwright install firefox
+        if file_name in string_dates_pool:
+            current_news.append(cur_news)
+
+    return current_news
+
+
+async def download_files(page, dates):
+    for date in dates:
+        async with page.expect_download() as download_info:
+            button = date.locator('a.button--purple').first
+            await button.click()
+            download = await download_info.value
+
+        await download.save_as(os.path.join(downloads_path, download.suggested_filename))
 
 
 async def main():
-    web = await Web.create(hide=False)
-    await web.download_timetable()
-    await web.download_timetable()
-    await web.close()
-    pass
+    await run(hide=True)
 
 
 if __name__ == '__main__':
--- scripts/data_updater.py
+++ scripts/data_updater.py
@@ -1,49 +1,36 @@
 import shutil
 from datetime import datetime
-
+from modules.logger.logger import logger
 import schedule
 import time
 import asyncio
 
 from modules.database_api.interaction.insert import insert_logs
-from modules.database_updater.downloader import Web
+from modules.database_updater import downloader
 from modules.database_updater import parser
 from modules.database_updater import cleaner
 from modules.images_updater import updater
 
-
-class DataUpdater:
-    def __init__(self, web):
-        self.web = web
-
-    async def run(self):
-        await self.web.download_timetable()
-        parser.parse_all()
-        cleaner.clean_all()
-        updater.update()
-
-    @classmethod
-    async def create(cls):
-        web = await Web.create()
-        return DataUpdater(web)
+@logger
+def run_data_update():
+    asyncio.run(downloader.run())
+    parser.parse_all()
+    cleaner.clean_all()
+    updater.update()
 
 
-async def data_update_run_once():
-    data_updater = await DataUpdater.create()
-    await data_updater.run()
+def data_update_run_once():
+    run_data_update()
 
 
-async def data_update_run_repeat():
-    delay = 2 * 60 * 100
-    data_updater = await DataUpdater.create()
-
+def data_update_run_repeat():
+    schedule.every(10).minutes.do(run_data_update)
     while True:
-        await data_updater.run()
-        time.sleep(delay)
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
     # data_update_run_once()
-    asyncio.run(data_update_run_repeat())
-
+    data_update_run_repeat()
     # run_repeat()
