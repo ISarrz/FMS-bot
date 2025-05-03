f1ead231955a0b66d80b59044eb9602decb4b7ca
German Zlobin <gezlobin@edu.hse.ru>
2025-05-03 03:14:48 +0300

    decompose user update 1.9.6

--- scripts/telegram_bot.py
+++ scripts/telegram_bot.py
@@ -91,25 +91,29 @@ async def update_user_info(context: CallbackContext):
     users = fetch_all_class_users()
 
     for user in users:
-        user_groups = fetch_user_groups_by_id(user.id)
-        user_groups = [fetch_class_group_by_id(group['id']) for group in user_groups]
-        updated_dates = []
-        for date in get_current_string_dates():
+        await update_user(context, user)
 
-            updated_groups = fetch_user_update_groups_by_date(user.id, date)
-            updated_groups = [fetch_class_group_by_id(group['id']) for group in updated_groups]
 
-            for group in user_groups:
-                if group not in updated_groups and fetch_image_id_by_date_and_group_id(date, group.id):
-                    updated_dates.append(date)
-                    insert_user_updates(user.id, date, group.id)
+async def update_user(context, user):
+    user_groups = fetch_user_groups_by_id(user.id)
+    user_groups = [fetch_class_group_by_id(group['id']) for group in user_groups]
+    updated_dates = []
+    for date in get_current_string_dates():
 
-        updated_dates = list(set(updated_dates))
+        updated_groups = fetch_user_update_groups_by_date(user.id, date)
+        updated_groups = [fetch_class_group_by_id(group['id']) for group in updated_groups]
 
-        notif_state = fetch_user_notifications(user.id)
-        if updated_dates and notif_state['value']:
-            text = 'Доступно расписание на: ' + ", ".join(updated_dates)
-            await context.bot.send_message(chat_id=user.telegram_id, text=text)
+        for group in user_groups:
+            if group not in updated_groups and fetch_image_id_by_date_and_group_id(date, group.id):
+                updated_dates.append(date)
+                insert_user_updates(user.id, date, group.id)
+
+    updated_dates = list(set(updated_dates))
+
+    notif_state = fetch_user_notifications(user.id)
+    if updated_dates and notif_state['value']:
+        text = 'Доступно расписание на: ' + ", ".join(updated_dates)
+        await context.bot.send_message(chat_id=user.telegram_id, text=text)
 
 
 async def get_chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
