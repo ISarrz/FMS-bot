2fed4b33cd19f00ae22ad29a8e859310f66a84cb
German Zlobin <gezlobin@edu.hse.ru>
2026-01-11 11:24:21 +0300

    aaa

--- scripts/make_database_backup.py
+++ scripts/make_database_backup.py
@@ -1,6 +1,6 @@
 from scripts.data_updater import make_database_backup
 from modules.database.database.database import DB
 if __name__ == "__main__":
-    pass
-    make_database_backup()
-    # DB.load_last_backup()
+    print(0)
+    DB.make_backup()
+    print(1)
